cc9c5345e0db7722f21de1b91142c5846e79fb1b
Max Wang <maxwang@dhcp-vl2041-13950.eduroam.cornell.edu>
2023-03-13 15:43:28 -0400

    added get_img_urls to return urls in json

--- tinder_api/test.py
+++ tinder_api/test.py
@@ -1,9 +1,30 @@
 from tinder_api_sms import *
-#print(get_recommendations())
 import pprint;
-printer = pprint.PrettyPrinter(indent = 4);
-recs = get_recommendations()["results"];
-for person in recs:
-    person_id = person["_id"];
-    person_bio = person["bio"];
-    print(person_bio);
+
+def get_img_urls():
+    recs = get_recs_v2()['data']['results']
+    photo_dict = {}
+    i = 0
+    for person in recs:
+        photo_dict[i] = recs[i]['user']['photos'][0]['url']
+        i += 1
+    #print(photo_dict)
+    return json.dumps(photo_dict)
+
+
+
+
+#json_str = json.dumps(photo_dict)
+#print(json_str)
+
+# writing to text.txt
+#with open("test.txt", 'w') as f:
+#    f.write(json_str)
+
+
+# format of json
+#json_str = json.dumps(get_recs_v2(), indent =3)
+#print(json_str)
+
+
+
