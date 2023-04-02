0dba48a7c406a4acd1a656d96f248aa4cb4ead28
Max Wang <maxwang@Maxs-MacBook-Pro-2.local>
2023-04-01 19:12:06 -0700

    download function

--- tinder_api/get_imgs.py
+++ tinder_api/get_imgs.py
@@ -1,6 +1,8 @@
 from tinder_api_sms import *
 import pprint;
+import urllib;
 
+#uses get_recs_v2() from features.py to return json with pfp urls 
 def get_img_urls():
     recs = get_recs_v2()['data']['results']
     photo_dict = {}
@@ -8,10 +10,15 @@ def get_img_urls():
     for person in recs:
         photo_dict[i] = recs[i]['user']['photos'][0]['url']
         i += 1
-    #print(photo_dict)
     return json.dumps(photo_dict)
 
-
+#attemps to download images into the images/unclassified folder
+def download_imgs():
+    recs = get_recs_v2()['data']['results']
+    i = 0
+    for person in recs:
+        urllib.request.urlretrieve(recs[i]['user']['photos'][0]['url'], 'tinderbot/images/unclassified/person{}.jpg'.format(i))
+        i += 1
 
 
 #json_str = json.dumps(photo_dict)
